4b315038ef2f52aabc94273b9fd3a138a686caa3
EOussama <oussama.essamadi@gmail.com>
2023-12-22 22:57:52 +0100

    Added Center First Window Patch

--- config.h
+++ config.h
@@ -14,7 +14,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#444444";
 static const unsigned int baralpha = 0x11;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
@@ -38,9 +38,8 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      	     instance    title    tags mask     isfloating   CenterThisWindow?     monitor */
+	{ "st",              NULL,       NULL,    0,            0,     	     1,		           -1 },
 };
 
 /* layout(s) */
