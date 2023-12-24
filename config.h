85eecee7b18cef5aaf40f3b2b5bfe60ddf97c4be
EOussama <oussama.essamadi@gmail.com>
2023-12-24 02:35:44 +0100

    Fixed Pixel Size

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad	    = 10;	/* vertical padding of bar */
 static const int sidepad	    = 10;	/* horizontal padding of bar */
-static const char *fonts[]          = { "JetBrains Mono:size=16" };
+static const char *fonts[]          = { "JetBrains Mono:pixelsize=16" };
 static const char dmenufont[]       = "JetBrains Mono:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
